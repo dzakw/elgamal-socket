0c653c9616574d04586c6eb88482f9077fc6d462
Iqbal Dzakwan I <41475575+dzakw@users.noreply.github.com>
2022-04-15 10:13:20 +0700

    penambahan message
    penambahan feedback message untuk koneksi yang terjadi

--- client.py
+++ client.py
@@ -2,17 +2,27 @@ import socket
 import elgamal
 import pickle
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
-client.connect(('localhost', 1002))  # 127.0.0.1
+client = socket.socket() #socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
 
-msg = input("msg: ")
+
+# the ip address or hostname of the server, the receiver
+host = socket.gethostname()
+# the port, let's use 5001
+port = 1024
+
+print(f"[+] Connecting to {host}:{port}")
+client.connect((host, port))  # 127.0.0.1
+print("[+] Connected.")
+
+msg = input("[+] msg: ")
 q=elgamal.q
 h=elgamal.h
 g=elgamal.g
 ct,p=elgamal.enc(msg,q,h,g)
-print("Sending encrypted msg...")
+print("[+] Sending encrypted msg...")
 client.send(pickle.dumps(ct))
-print("msg sent.")
-print("Encrypted msg=",ct)
+print("[+] msg sent.")
+print("[+] Encrypted msg=",ct)
+print("[+] Connection closed.")
 
 client.close()
--- server.py
+++ server.py
@@ -2,15 +2,21 @@ import socket
 import elgamal
 import pickle
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
-server.bind(('localhost', 1002))  # 127.0.0.1
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 1024
+
+server = socket.socket() #socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
+server.bind((SERVER_HOST, SERVER_PORT))  # 127.0.0.1
 server.listen()
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
 client_socket, client_address = server.accept()
+print(f"[+] {client_address} is connected.")
 
 ct = pickle.loads(client_socket.recv(2048))
-print("Receiving text...")
-print("encrypted message: ", end="")
+print("[+] Receiving text...")
+print("[+] encrypted message: ", end="")
 print(ct)
 
 def lts(s): 
@@ -23,7 +29,7 @@ p=elgamal.p
 key=elgamal.key
 q=elgamal.q
 pt=elgamal.dec(ct,p,key,q)
-print("decrypted message: ", end="")
+print("[+] decrypted message: ", end="")
 print(lts(pt))
 
 client_socket.close()
